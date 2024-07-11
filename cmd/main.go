0be0533d3f1fbe325b870acdbc1caef4622e76ea
sam hobson <shhobson@proton.me>
2024-07-11 17:34:56 +1000

    Added form submission with validation

--- cmd/main.go
+++ cmd/main.go
@@ -25,8 +25,64 @@ func newTempalte() *Templates {
 	}
 }
 
-type Count struct {
-	Count int
+type Contact struct {
+	Name  string
+	Email string
+}
+
+func newContact(name string, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func (d *Data) emailExists(email string) bool {
+	for _, c := range d.Contacts {
+		if c.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("Steve", "steve@baboonmail.com"),
+			newContact("Sam", "sam@proton.me"),
+		},
+	}
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
 }
 
 func main() {
@@ -35,16 +91,29 @@ func main() {
 
 	e.Renderer = newTempalte()
 
-	count := Count{Count: 0}
+	page := newPage()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		return c.Render(200, "index", page)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count += 1
-		return c.Render(200, "count", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		if page.Data.emailExists(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+
+			return c.Render(422, "form", formData)
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts, newContact(name, email))
+
+		return c.Render(200, "display", page.Data)
 	})
 
-    e.Logger.Fatal(e.Start(":42069"))
+	e.Logger.Fatal(e.Start(":42069"))
 }
